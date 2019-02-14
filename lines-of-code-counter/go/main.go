fab966000fc99dee3cab3752f7befb273496112e
zsusyt <17499032@qq.com>
2019-02-15 00:18:44 +0800

    finish go

--- lines-of-code-counter/go/main.go
+++ lines-of-code-counter/go/main.go
@@ -8,8 +8,19 @@ import (
 	"path/filepath"
 )
 
+var config map[string]bool
+var sum int64
+
+func init () {
+	sum = 0
+	config = make(map[string]bool)
+	config["js"] = true
+	config["vue"] = true
+}
+
 func main () {
-	projectPath := flag.String("pp", "E:\\sourceCode\\isp-fe\\src", "请提供想要统计的工程路径")
+	//projectPath := flag.String("pp", "E:\\sourceCode\\isp-fe\\src", "请提供想要统计的工程路径")
+	projectPath := flag.String("pp", "/Users/zsusyt/works/lvshou/isp-fe/src/util", "请提供想要统计的工程路径")
 	flag.Parse()
 	fmt.Println(*projectPath)
 
@@ -18,32 +29,53 @@ func main () {
 		fmt.Println("读取工程路径出错")
 	}
 
-	fmt.Println("fi is a Dir:", fi.IsDir())
-
 	files, err := ioutil.ReadDir(*projectPath)
 	if err != nil {
 		fmt.Println("读取工程路径内的文件出错")
 	}
 
 	for _, file := range files {
-		if file.IsDir() {
-			fmt.Println("=====================")
-			absFilePath, err := filepath.Abs(file.Name())
-			fmt.Println(absFilePath)
-			files, err := ioutil.ReadDir(absFilePath)
-			if err != nil {
-				fmt.Println("读取子文件夹出错")
-			}
-
-			for _, file := range files {
-				fmt.Println(file.Name(),"is a Dir?", file.IsDir())
-			}
-		} else {
-			fmt.Println(file.Name(),"is a file.")
-		}
-
+		readChild(*projectPath, file)
 	}
+
+	fmt.Println("总行数为:", sum)
 
 }
 
-//func calcDirLines
+func readChild(parent string, child os.FileInfo) {
+
+	newPath := filepath.Join(parent, child.Name())
+	if child.IsDir() {
+		fmt.Println(newPath)
+
+		files, _ := ioutil.ReadDir(newPath)
+		for _, file := range files {
+			readChild(newPath, file)
+		}
+	} else {
+		//fmt.Println(child.Name(),"is a file.")
+		ext := filepath.Ext(child.Name())[1:]
+		if config[ext] {
+			//读取文件，计算行数加到sum上
+			count, err := countFileLine(newPath)
+			if err != nil {
+				fmt.Println("读取文件时出错")
+			}
+			sum += count
+		}
+	}
+}
+
+func countFileLine(name string) (count int64, err error) {
+	data, err := ioutil.ReadFile(name)
+	if err != nil {
+		return
+	}
+	count = 0
+	for i := 0; i < len(data); i++ {
+		if data[i] == '\n' {
+			count++
+		}
+	}
+	return
+}
