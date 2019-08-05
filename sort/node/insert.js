24df8df5ea9d956d6f52ae5cd05389cb5ccce3cc
zsusyt <17499032@qq.com>
2019-08-05 15:45:24 +0800

    add sort dir

---
+++ sort/node/insert.js
@@ -0,0 +1,32 @@
+function insert(data) {
+    let length = data.length;
+    if(length <= 1) {
+        return data
+    }
+
+    for(let i = 1; i < length; i++){
+        help(data, i)
+    }
+
+    return data;
+}
+
+function help(data, index) {
+    if(index > 0) {
+        if(data[index] < data[index-1]) {
+            swap(data, index-1, index)
+            help(data, index - 1)
+        }
+    }
+}
+
+function swap(data, dstIndex, srcIndex) {
+    let tmp = data[dstIndex];
+    data[dstIndex] = data[srcIndex];
+    data[srcIndex] = tmp
+}
+
+let a = [1,2,4,3]
+
+let result = insert(a);
+console.log(result)
