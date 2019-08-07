6295d5d61584a7c9f206fa3a75af486b1d412dce
zsusyt <17499032@qq.com>
2019-08-07 09:29:28 +0800

    revise merge

--- sort/node/merge.js
+++ sort/node/merge.js
@@ -1,41 +1,44 @@
-function merge(data, left, right) {
+function merge(data, left, right, tmpArray) {
     if((right - left) > 0) {
-        let tmpArray = [];
-
         let mid = Math.floor((left + right) / 2);
         merge(data, left, mid, tmpArray);
         merge(data, mid + 1, right, tmpArray);
-        let i = 0; 
-        let iL = left;
-        let iR = mid + 1;
-        while(iL <= mid && iR <= right) {
-            if(data[iL] < data[iR]) {
-                tmpArray[i] = data[iL];
-                iL++
-            } else {
-                tmpArray[i] = data[iR];
-                iR++
-            }
-            i++;
-        };
+
+        mergeArray(data, left, mid, right, tmpArray)
         
-        while(iL<=mid){
-            tmpArray[i] = data[iL];
-            iL++;
-            i++
-        }
-
-        while(iR<=right){
-            tmpArray[i] = data[iR];
-            iR++;
-            i++
-        }
-
-        data.splice(left, (right - left + 1), ...tmpArray)
     }
 }
 
-let a = [5, 4, 3, 1, 20, 35]
+function mergeArray(data, left, mid, right, tmpArray) {
+    let i = 0; 
+    let iL = left;
+    let iR = mid + 1;
+    while(iL <= mid && iR <= right) {
+        if(data[iL] < data[iR]) {
+            tmpArray[i] = data[iL];
+            iL++
+        } else {
+            tmpArray[i] = data[iR];
+            iR++
+        }
+        i++;
+    };
+    
+    while(iL<=mid){
+        tmpArray[i] = data[iL];
+        iL++;
+        i++
+    }
 
-merge(a, 0, a.length-1);
+    while(iR<=right){
+        tmpArray[i] = data[iR];
+        iR++;
+        i++
+    }
+    data.splice(left, (right - left + 1), ...tmpArray.slice(0, right - left + 1))
+}
+let tmpArray = [];
+let a = [5, 4, 3, 1, 20, 35, 2, 8, 9, 2]
+
+merge(a, 0, a.length-1, tmpArray);
 console.log(a)
